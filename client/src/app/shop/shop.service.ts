3f4cdb3959c0a94594c89c91e5ea35c088b5f131
Sampeur Samuel Louis <sampeur4life1@gmail.com>
2024-06-30 17:58:10 -0700

    Setting up routes, lazy loading and active nav links

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -26,6 +26,10 @@ export class ShopService {
     return this.http.get<Pagination<IProduct[]>>(this.baseUrl + 'products', { params })
   }
 
+  getProduct(id: any){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
   }
